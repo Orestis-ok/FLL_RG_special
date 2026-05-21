bb9e0742ca9c5c149a2b1945ab002a0cc07ea8bf
Orestis K <okerkines@gmail.com>
2026-05-21 22:59:28 +0300

    Update Route1.py

--- Route1.py
+++ Route1.py
@@ -12,3 +12,11 @@ from MyLibrary import *
 def Route1(): 
     
     PortView_Battery()
+    
+    # MoveStraight_Distance (300,300,450,True,True,Stop.BRAKE)
+    # PointTurn_Angle (300, 300, 90, True, Stop.BRAKE)
+    # MoveSteering_Seconds(300, 20, 300)
+    # leftArm.run_time(300, 300, then=Stop.BRAKE, wait=True)
+
+    MoveSteering_Seconds(600, 20, 1000)
+    MoveSteering_Seconds(600, 10, 450)
