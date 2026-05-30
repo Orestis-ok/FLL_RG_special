694d21aa86e8640ee59696bde87bacbab8b14c3a
Orestis K <okerkines@gmail.com>
2026-05-30 21:40:52 +0300

    Update Route2.py

--- Route2.py
+++ Route2.py
@@ -1,4 +1,4 @@
-####################### Pyricks library ########################
+######################## Pyricks library ########################
 
 from pybricks.parameters import Color, Direction, Stop, Icon
 from pybricks.tools import wait, StopWatch, Matrix
@@ -7,16 +7,18 @@ from pybricks.tools import wait, StopWatch, Matrix
 
 from MyLibrary import *
 
-
+  
 ####################### Route program ########################
-def Route3(): 
-	
-    PortView_Battery()
+def Route2(): 
     
+    PortView_Battery()
     MoveSteering_Seconds(-50, 0, 300)
-	rightArm.run_time(-400,400 , then=Stop.BRAKE, wait=False)
 	MoveStraight_Distance (600,400,725,True,True,Stop.BRAKE)
     PointTurn_Angle (300, 300, -45, True, Stop.BRAKE)
     MoveStraight_Distance (450,300,150,True,True,Stop.BRAKE)
     rightArm.run_time(400, 700, then=Stop.BRAKE, wait=True)
-    MoveStraight_Distance (300,300,-200,True,True,Stop.BRAKE)
+    MoveStraight_Distance (300,300,-150,True,True,Stop.BRAKE)
+    PointTurn_Angle (300, 300,50, True, Stop.BRAKE)
+    MoveStraight_Distance (300,300,-150,True,True,Stop.BRAKE)
+    MoveStraight_Distance (300,300,80,True,True,Stop.BRAKE)
+    leftArm.run_time(-950, 2000, then=Stop.BRAKE, wait=True)
